f68f3eedced9c80669685ae410d4a503beac72f7
Ranjoy <zico1439@gmail.com>
2022-03-13 21:17:31 +0100

    updated output

--- MavenCMD3/src/test/java/ui/demo3.java
+++ MavenCMD3/src/test/java/ui/demo3.java
@@ -12,6 +12,6 @@ public class demo3 {
 	
 	@Test(groups = {"regression"}) 		// mentioning test group
 	public void example6() {
-		System.out.println("### Output6 ###");
+		System.out.println("#### Output6 ####");
 	}
 }
